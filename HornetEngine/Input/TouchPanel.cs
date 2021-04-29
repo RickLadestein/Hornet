5e21fbc5c5f911f0306b9f6b64e7455562ea276e
Rick Ladestein <rsladest@avans.nl>
2021-04-29 10:50:43 +0200

    [Added] touch info logging

--- HornetEngine/Input/TouchPanel.cs
+++ HornetEngine/Input/TouchPanel.cs
@@ -23,16 +23,16 @@ namespace HornetEngine.Input
 
     public struct TOUCH_STRUCT
     {
-        long x;
-        long y;
-        IntPtr hSource;
-        ulong dwID;
-        ulong dwFlags;
-        ulong dwMask;
-        ulong dwTime;
-        IntPtr dwExtraInfo;
-        ulong cxContact;
-        ulong cyContact;
+        public Int32 x;
+        public Int32 y;
+        public IntPtr hSource;
+        public UInt32 dwID;
+        public UInt32 dwFlags;
+        public UInt32 dwMask;
+        public UInt32 dwTime;
+        public Int64 dwExtraInfo;
+        public UInt32 cxContact;
+        public UInt32 cyContact;
     }
 
     class TouchPanel : IDisposable
@@ -43,13 +43,17 @@ namespace HornetEngine.Input
         private static IntPtr oldproc;
 
         private static readonly uint WM_TOUCH = 0x0240;
+        private static readonly uint TOUCH_DOWN = 0x0002;
+        private static readonly uint TOUCH_UP = 0x0004;
 
         private bool bound;
         private bool registered_touch;
+        private static Dictionary<UInt32, TOUCH_STRUCT> dict;
 
         delegate IntPtr WndProcDelegate(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam);
         public unsafe TouchPanel(Glfw api, ref GlfwNativeWindow wnd)
         {
+            dict = new Dictionary<uint, TOUCH_STRUCT>();
             GetWindowPtr(ref wnd);
             LinkToWin32();
             //if (wnd.Win32.HasValue)
@@ -67,7 +71,7 @@ namespace HornetEngine.Input
         {
             if (wnd.Win32.HasValue)
             {
-                IntPtr HWnd = wnd.Win32.Value.Hwnd;
+                HWnd = wnd.Win32.Value.Hwnd;
             } else
             {
                 throw new Exception("Could not capture native window ptr");
@@ -89,23 +93,55 @@ namespace HornetEngine.Input
         {
             if(msg == WM_TOUCH)
             {
-                Console.WriteLine("Touch Event");
+                //Console.WriteLine("Touch Event");
                 long count = wparam.ToInt64();
-                IntPtr test = Marshal.AllocHGlobal((int)(sizeof(TOUCH_STRUCT) * count));
-                bool succes = GetTouchInputInfo(lparam, (uint)count, test, (uint)count);
+                int arrsize = (int)(sizeof(TOUCH_STRUCT) * count);
+                IntPtr test = Marshal.AllocHGlobal(arrsize);
+                bool succes = GetTouchInputInfo(lparam, (uint)count, test, (uint)sizeof(TOUCH_STRUCT));
+                long error = GetLastError();
                 if(succes)
                 {
-                    List<TOUCH_STRUCT> touch_events = new List<TOUCH_STRUCT>();
+                    //List<TOUCH_STRUCT> touch_events = new List<TOUCH_STRUCT>();
                     TOUCH_STRUCT* ptr = (TOUCH_STRUCT*) test.ToPointer();
                     for(int i = 0; i < (int)count; i++)
                     {
                         TOUCH_STRUCT ts = ptr[i];
-                        touch_events.Add(ts);
+                        //touch_events.Add(ts);
+                        UInt32 down_result = ts.dwFlags & TOUCH_DOWN;
+                        UInt32 up_result = ts.dwFlags & TOUCH_UP;
+                        if(down_result > 0)
+                        {
+                            dict[ts.dwID] = ts;
+                        } else if(up_result > 0)
+                        {
+                            TOUCH_STRUCT tmp;
+                            bool ss = dict.TryGetValue(ts.dwID, out tmp);
+                            if(ss)
+                            {
+                                dict.Remove(ts.dwID);
+                            }
+                        }
                     }
                     CloseTouchInputHandle(lparam);
+                    if (dict.Values.Count != 0)
+                    {
+                        Console.CursorLeft = 0;
+                        Console.CursorTop = 0;
+                        Console.WriteLine("-----------------------------------------------------------------");
+                        foreach (TOUCH_STRUCT ts in dict.Values)
+                        {
+                            Console.WriteLine($"Touch event id:{ts.dwID}  X:{ts.x} Y:{ts.y}");
+                        }
+                        Console.WriteLine("-----------------------------------------------------------------");
+                    }
                 }
             }
             return (IntPtr)CallWindowProc(oldproc, hwnd, msg, wparam, lparam);
+        }
+
+        bool IsBitSet(byte b, int pos)
+        {
+            return (b & (1 << pos)) != 0;
         }
 
         [DllImport("user32.dll", SetLastError = true)]
@@ -125,6 +161,9 @@ namespace HornetEngine.Input
 
         [DllImport("user32.dll", SetLastError = true)]
         static extern bool CloseTouchInputHandle(IntPtr hTouchInput);
+
+        [DllImport("Kernel32.dll", SetLastError = true)]
+        static extern long GetLastError();
 
         public void Dispose()
         {
