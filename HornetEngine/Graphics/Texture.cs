7181673256f761af2a3f55ce493f0d5a0eb8a893
Rick_Desktop <rsladest@avans.nl>
2021-05-11 16:09:23 +0200

    [Added] basic multitexture and image

---
+++ HornetEngine/Graphics/Texture.cs
@@ -0,0 +1,104 @@
+﻿using Silk.NET.OpenGL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HornetEngine.Graphics
+{
+
+    public enum HTextureUnit : uint
+    {
+        Unit_0,
+        Unit_1,
+        Unit_2,
+        Unit_3,
+        Unit_4,
+        Unit_5,
+        Unit_6,
+        Unit_7
+    }
+    public class Texture
+    {
+        public uint Handle;
+
+
+        public void Bind()
+        {
+
+        }
+
+        public void Unbind()
+        {
+
+        }
+    }
+
+    public class MultiTexture
+    {
+        public static readonly uint MAX_TEXTURE_LAYERS = 8;
+        public static MultiTexture current_bound;
+        public Texture[] textures;
+
+        public MultiTexture()
+        {
+            textures = new Texture[MAX_TEXTURE_LAYERS];
+        }
+
+        public void Bind()
+        {
+            if(current_bound != null)
+            {
+                current_bound.Unbind();
+            }
+            current_bound = this;
+            uint min = (uint)GLEnum.Texture0;
+            uint max = (uint)GLEnum.Texture0 + (MAX_TEXTURE_LAYERS - 1);
+            uint tex_id = 0;
+            for (uint i = min; i < max; i++)
+            {
+                NativeWindow.GL.ActiveTexture((GLEnum)i);
+                if(this.textures[tex_id] != null)
+                {
+                    NativeWindow.GL.BindTexture(GLEnum.Texture2D, textures[tex_id].Handle);
+                }
+                tex_id += 1;
+            }
+            NativeWindow.GL.ActiveTexture(GLEnum.Texture0);
+        }
+
+        public void Unbind()
+        {
+            if(current_bound == this)
+            {
+                uint min = (uint)GLEnum.Texture0;
+                uint max = (uint)GLEnum.Texture0 + (MAX_TEXTURE_LAYERS - 1);
+                for(uint i = min; i < max; i++)
+                {
+                    NativeWindow.GL.ActiveTexture((GLEnum)i);
+                    NativeWindow.GL.BindTexture(GLEnum.Texture2D, 0);
+                }
+                current_bound = null;
+            }
+        }
+
+        public void SetTextureUnit(Texture tex, HTextureUnit layer)
+        {
+            uint loc = (uint)layer;
+            if(loc >= MAX_TEXTURE_LAYERS)
+            {
+                throw new IndexOutOfRangeException("Texture unit was out of the range of the MultiTexture texture array");
+            }
+            this.textures[(uint)layer] = tex;
+        }
+
+        public void ClearTextureUnit(HTextureUnit layer)
+        {
+            uint loc = (uint)layer;
+            if (loc >= MAX_TEXTURE_LAYERS)
+            {
+                throw new IndexOutOfRangeException("Texture unit was out of the range of the MultiTexture texture array");
+            }
+            this.textures[(uint)layer] = null;
+        }
+    }
+}
---
+++ HornetEngine/Util/ImageResource.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using HornetEngine.Graphics;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp;
+using Silk.NET.OpenGL;
+
+namespace HornetEngine.Util
+{
+    public class ImageResource : IDisposable
+    {
+        Image<Rgba32> image;
+        uint width, height;
+
+        public ImageResource(SixLabors.ImageSharp.Image<Rgba32> im)
+        {
+            if(im == null)
+            {
+                throw new ArgumentException("Image cannot be null");
+            }
+
+            this.image = im;
+            width = (uint)im.Width;
+            height = (uint)im.Height;
+        }
+
+        public unsafe bool LoadToTexture(HornetEngine.Graphics.Texture tex)
+        {
+            tex.Bind();
+            fixed (void* data = &MemoryMarshal.GetReference(image.GetPixelRowSpan(0)))
+            {
+                NativeWindow.GL.TexImage2D(TextureTarget.Texture2D, 0, (int)InternalFormat.Rgba, width, height, 0, PixelFormat.Rgba, PixelType.UnsignedByte, data);
+                return true;
+            }
+        }
+
+        public static ImageResource Load(string path, bool flip)
+        {
+            try
+            {
+                SixLabors.ImageSharp.Image<Rgba32> im = (SixLabors.ImageSharp.Image<Rgba32>)SixLabors.ImageSharp.Image.Load(path);
+                if(flip)
+                {
+                    im.Mutate(x => x.Flip(FlipMode.Vertical));
+                }
+                return new ImageResource(im);
+            } catch(Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public void Dispose()
+        {
+            if(image != null)
+            {
+                image.Dispose();
+            }
+        }
+    }
+}
