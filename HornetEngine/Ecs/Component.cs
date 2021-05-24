7ace1079d45d70cd06396affdbd29989f76f8af6
Rick Ladestein <rsladest@avans.nl>
2021-05-24 13:27:31 +0200

    [Added] basic Entity Component System

---
+++ HornetEngine/Ecs/Component.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HornetEngine.Ecs
+{
+    public abstract class Component
+    {
+        public Guid id { get; private set; }
+        public Component()
+        {
+            id = Guid.NewGuid();
+        }
+
+        public abstract new String ToString();
+    }
+}
---
+++ HornetEngine/Ecs/MonoScript.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HornetEngine.Ecs
+{
+    public abstract class MonoScript
+    {
+
+        public abstract void Start();
+
+        public abstract void Update();
+
+    }
+}
