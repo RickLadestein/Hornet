3841bac8d2925a3679ce88d248c127429b7004b5
CinqueIzumi <ba.peereboom@gmail.com>
2021-06-24 13:04:11 +0200

    [Added] Config and json classes

---
+++ HornetEngine/Configuration/JsonRoot.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HornetEngine.Configuration
+{
+    public class JsonRoot
+    {
+        public JsonData data { get; set; }
+        public List<JsonRule> rules { get; set; }
+    }
+}
