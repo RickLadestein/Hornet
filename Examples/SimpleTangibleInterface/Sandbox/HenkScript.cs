c0bf25bc5a15c24edde0f1a546343d577f713cac
Rick Ladestein <rsladest@avans.nl>
2021-06-29 13:04:24 +0200

    [Added] simple tangible interface example

---
+++ Examples/SimpleTangibleInterface/Sandbox/HenkScript.cs
@@ -0,0 +1,100 @@
+﻿using HornetEngine.Ecs;
+using HornetEngine.Graphics;
+using HornetEngine.Input.Touch_Recognition;
+using HornetEngine.Sound;
+using HornetEngine.Util;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sandbox
+{
+    public class HenkScript : MonoScript
+    {
+        private SoundSourceComponent scomp;
+        private MaterialComponent mcomp;
+
+        public override void Start()
+        {
+            base.Start();
+
+            scomp = this.entity.GetComponent<SoundSourceComponent>();
+            mcomp = this.entity.GetComponent<MaterialComponent>();
+
+            if (scomp == null || mcomp == null)
+            {
+                throw new Exception("Component was not initialized");
+            }
+
+            mcomp.SetTextureUnit("qmark", HTextureUnit.Unit_0);
+        }
+
+        public override void Update()
+        {
+            base.Update();
+            TouchManager.Instance.Refresh();
+            try
+            {
+                List<TouchObject> objects = TouchManager.Instance.GetTouchObjects();
+                TouchObject current_object = objects[0];
+                
+                switch(current_object.type)
+                {
+                    case TouchPointType.TYPE1:
+                        HandleDrum();
+                        break;
+                    case TouchPointType.TYPE2:
+                        HandleViolin();
+                        break;
+                    case TouchPointType.TYPE3:
+                        HandleGuitar();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid type");
+                        break;
+                }
+            } catch
+            {
+            }
+        }
+
+        private void HandleDrum()
+        {
+            if (!scomp.IsPlaying)
+            {
+                scomp.PlaySoundEffect(SoundResourceManager.Instance.GetResource("drum"), 1.0f, 1.0f);
+            }
+
+            if (mcomp.Textures.textures[0] != TextureResourceManager.Instance.GetResource("drum"))
+            {
+                mcomp.SetTextureUnit("drum", HTextureUnit.Unit_0);
+            }
+        }
+
+        private void HandleViolin()
+        {
+            if (!scomp.IsPlaying)
+            {
+                scomp.PlaySoundEffect(SoundResourceManager.Instance.GetResource("violin"), 1.0f, 1.0f);
+            }
+
+            if (mcomp.Textures.textures[0] != TextureResourceManager.Instance.GetResource("violin"))
+            {
+                mcomp.SetTextureUnit("violin", HTextureUnit.Unit_0);
+            }
+        }
+
+        private void HandleGuitar()
+        {
+            if (!scomp.IsPlaying)
+            {
+                scomp.PlaySoundEffect(SoundResourceManager.Instance.GetResource("guitar"), 1.0f, 1.0f);
+            }
+
+            if (mcomp.Textures.textures[0] != TextureResourceManager.Instance.GetResource("guitar"))
+            {
+                mcomp.SetTextureUnit("guitar", HTextureUnit.Unit_0);
+            }
+        }
+    }
+}
---
+++ Examples/SimpleTangibleInterface/Sandbox/Program.cs
@@ -0,0 +1,99 @@
+using System;
+using HornetEngine.Ecs;
+using HornetEngine.Graphics;
+using HornetEngine.Graphics.Buffers;
+using HornetEngine.Util;
+using HornetEngine.Input.Touch_Recognition;
+using System.Numerics;
+using HornetEngine;
+using System.Threading.Tasks;
+using HornetEngine.Input;
+using HornetEngine.Sound;
+
+namespace Sandbox
+{
+    class Program
+    {
+        public static Window w = new Window();
+        public static Entity line_entity;
+        public static Entity player;
+
+        static void Main()
+        {
+            setupGraphicsDemo();
+        }
+
+        private static void setupGraphicsDemo()
+        {
+            DirectoryManager.RegisterResourceDir("textures", "resources\\textures");
+            DirectoryManager.RegisterResourceDir("shaders", "resources\\shaders");
+            DirectoryManager.RegisterResourceDir("models", "resources\\models");
+            DirectoryManager.RegisterResourceDir("samples", "resources\\samples");
+
+            SoundResourceManager mgr = SoundResourceManager.Instance;
+
+            w.Open("Test", 1920, 1080, WindowMode.WINDOWED);
+            w.Title = "Helloworld";
+
+            DoManualResourceAquisition();
+
+            //player = new Entity("Player");
+            //PlayerScript pscr = new PlayerScript
+            //{
+            //    mouse = w.Mouse,
+            //    keyboard = w.Keyboard
+            //};
+            //player.AddScript(pscr);
+
+            //player.AddComponent(new AudioListenerComponent());
+            //Scene.Instance.AddEntity(player);
+
+
+            Entity henk = new Entity("plane");
+            HenkScript hs = new HenkScript();
+            henk.AddScript(hs);
+
+            SoundSourceComponent ssc = new SoundSourceComponent();
+            henk.AddComponent(ssc);
+
+            Mesh mesh = Mesh.ImportMesh("planemesh", "models", "plane.obj");
+            MeshResourceManager.Instance.AddResource("planemesh" , mesh);
+            MeshComponent ms = new MeshComponent();
+            ms.SetTargetMesh("planemesh");
+            henk.AddComponent(ms);
+
+            InterfaceRenderComponent mrc = new InterfaceRenderComponent();
+            henk.AddComponent(mrc);
+
+            MaterialComponent matcomp = new MaterialComponent();
+            matcomp.SetShaderFromId("default");
+            //matcomp.SetTextureUnit("drum", HTextureUnit.Unit_0);
+            henk.AddComponent(matcomp);
+
+            henk.Transform.Position = new GlmSharp.vec3(0.0f, 0.0f, 20.0f);
+            henk.Transform.Scale = new GlmSharp.vec3(5.0f, 5.0f, 5.0f);
+            henk.Transform.Rotate(new GlmSharp.vec3(0.0f, 0.0f, 1.0f), 90);
+            Scene.Instance.AddEntity(henk);
+
+            w.Run();
+        }
+
+        #region MANUAL_RESOURCE
+        private static void DoManualResourceAquisition()
+        {
+            SoundResourceManager.Instance.ImportResource("bonk", "samples", "menu.wav");
+            SoundResourceManager.Instance.ImportResource("drum", "samples", "drum_kick.wav");
+            SoundResourceManager.Instance.ImportResource("guitar", "samples", "guitar_acoustic.wav");
+            SoundResourceManager.Instance.ImportResource("violin", "samples", "violin_c4.wav");
+
+            Config config = Config.Instance;
+
+            TextureResourceManager.Instance.ImportResource("default", "textures", "laminate1.png");
+            TextureResourceManager.Instance.ImportResource("drum", "textures", "drum.png");
+            TextureResourceManager.Instance.ImportResource("guitar", "textures", "guitar.png");
+            TextureResourceManager.Instance.ImportResource("violin", "textures", "violin.png");
+            TextureResourceManager.Instance.ImportResource("qmark", "textures", "qmark.png");
+        }
+        #endregion
+    }
+}
